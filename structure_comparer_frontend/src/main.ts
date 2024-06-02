bafb6040069dce1f3afcade1a1dcdeee17dab6f5
Robert Hoffmann <rob.hoffmann@posteo.de>
2024-06-02 10:55:35 +0200

    solved errors and added css

--- structure_comparer_frontend/src/main.ts
+++ structure_comparer_frontend/src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
@@ -9,6 +10,6 @@ bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
-    { provide: BrowserAnimationsModule }
+    importProvidersFrom(BrowserAnimationsModule)
   ]
 }).catch(err => console.error(err));
