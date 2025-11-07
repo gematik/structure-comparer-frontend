b2c01a9679481453459f0f92324616ed5e1810f9
nikohl-de <nils.kohl@gematik.de>
2025-11-07 15:35:40 +0100

    added copyright comment

--- src/app/package-upload-dialog/package-upload-dialog.component.ts
+++ src/app/package-upload-dialog/package-upload-dialog.component.ts
@@ -1,3 +1,23 @@
+﻿/*
+ *
+ * Copyright 2025 gematik GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * *******
+ *
+ * For additional notes and disclaimer from gematik and in case of changes by gematik find details in the "Readme" file.
+ */
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { PackageService } from '../package.service';
@@ -55,7 +75,7 @@ export class PackageUploadDialogComponent {
       this.packageService.createPackage(this.projectKey, this.selectedFile)
     );
     console.log('Upload successful:', response);
-    this.dialogRef.close(response); // ← response statt file
+    this.dialogRef.close(response); // â† response statt file
     return response;
   } catch (err) {
     console.error('Upload failed:', err);
